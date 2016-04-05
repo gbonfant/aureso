55c1863c6f2acbc4e0d1bbba1974207f657ce962
Gus Bonfant <gbonfant@me.com>
2016-04-05 14:26:55 +0100

    Add serializer for Model
    Using activemodel-serializer for easier model serialization

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,5 +1,8 @@
 class ModelsController < ApplicationController
+
   def model_types
-    render json: :ok
+    models = Model.where(model_slug: params['model_slug'])
+
+    render json: models
   end
 end
--- spec/controllers/models_controller_spec.rb
+++ spec/controllers/models_controller_spec.rb
@@ -2,10 +2,26 @@ require 'rails_helper'
 
 describe ModelsController do
   describe 'GET models/:model_slug/model_types' do
-    it 'returns a list of models and models types' do
+    it 'returns a successful response' do
       get :model_types, { model_slug: 'foocar' }
 
       expect(response).to be_success
+    end
+
+    it 'returns a list of models and models types' do
+      Model.create(name: 'serie1', model_slug: 'foocar')
+      Model.create(name: 'serie2', model_slug: 'foocar')
+
+      get :model_types, { model_slug: 'foocar' }
+
+      res = JSON.parse(response.body, symbolize_names: true)
+
+      expect(res).to eq({
+        models: [
+          { name: 'serie1' },
+          { name: 'serie2' }
+        ]
+      })
     end
 
     it 'returns a total_price for each model type'
